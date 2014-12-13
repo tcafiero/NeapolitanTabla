29ba9d334f033c1632d9a036f2a45c2c3d8916aa
Toni Cafiero <toni@cafiero.it>
2014-12-13 12:12:12 +0100

    Versione di prova per riprogrammazione canali midi. Test ok. Riprogrammazione possibile

--- Translator/Translator/Translator.ino
+++ Translator/Translator/Translator.ino
@@ -7,7 +7,7 @@
 
 
 static enum midi::MidiType type;
-static midi::DataByte note, data2;
+static midi::DataByte data1, data2;
 static midi::Channel channel;
 
 
@@ -15,7 +15,7 @@ MIDI_CREATE_INSTANCE(HardwareSerial, Serial,     midiA);
 
 void setup()
 {
-  midiA.begin(0);
+  midiA.begin(MIDI_CHANNEL_OMNI);
   midiA.turnThruOff();
 }
 
@@ -30,109 +30,124 @@ void loop()
   if (midiA.read())
   {
     type = midiA.getType();
-    note = midiA.getData1();
+    data1 = midiA.getData1();
     data2 = midiA.getData2();
     channel = midiA.getChannel();
-    if ((type == midi::NoteOff) || (type == midi::NoteOn))
+    switch (type)
     {
-      switch (channel)
-      {
-        case 0x2:  switch (note)
+      case midi::NoteOff:
+      case midi::NoteOn:
+        {
+          switch (channel)
           {
-            case 0x37:
-              note = 0x2B;
-              channel = 0x2;
+            case 0x2:  switch (data1)
+              {
+                case 0x37:
+                  data1 = 0x2B;
+                  channel = 0x2;
+                  break;
+                case 0x48:
+                  data1 = 0x3C;
+                  channel = 0x2;
+                  break;
+                case 0x52:
+                  data1 = 0x46;
+                  channel = 0x2;
+                  break;
+                case 0x60:
+                  data1 = 0x54;
+                  channel = 0x5;
+                  break;
+                default: break;
+              };
               break;
-            case 0x48:
-              note = 0x3C;
-              channel = 0x2;
+            case 0x3:  switch (data1)
+              {
+                case 0x18:
+                  data1 = 0x0C;
+                  channel = 0x2;
+                  break;
+                case 0x37:
+                  data1 = 0x2A;
+                  channel = 0x3;
+                  break;
+                case 0x3C:
+                  data1 = 0x30;
+                  channel = 0x3;
+                  break;
+                case 0x48:
+                  data1 = 0x3C;
+                  channel = 0x3;
+                  break;
+                default: break;
+              };
               break;
-            case 0x52:
-              note = 0x46;
-              channel = 0x2;
+            case 0x4:  switch (data1)
+              {
+                case 0x11:
+                  data1 = 0x04;
+                  channel = 0x4;
+                  break;
+                case 0x13:
+                  data1 = 0x07;
+                  channel = 0x4;
+                  break;
+                case 0x24:
+                  data1 = 0x18;
+                  channel = 0x4;
+                  break;
+                case 0x30:
+                  data1 = 0x24;
+                  channel = 0x5;
+                  break;
+                default: break;
+              };
               break;
-            case 0x60:
-              note = 0x54;
-              channel = 0x5;
+            case 0x5:  switch (data1)
+              {
+                case 0x30:
+                  data1 = 0x24;
+                  channel = 0x3;
+                  break;
+                case 0x3E:
+                  data1 = 0x32;
+                  channel = 0x5;
+                  break;
+                case 0x4E:
+                  data1 = 0x42;
+                  channel = 0x5;
+                  break;
+                default: break;
+              };
+              break;
+            case 0xC:
+              switch (data1)
+              {
+                case 0x0C:
+                  data1 = 0x00;
+                  channel = 0x4;
+                  break;
+                default: break;
+              };
               break;
             default: break;
           };
-          break;
-        case 0x3:  switch (note)
-          {
-            case 0x18:
-              note = 0x0C;
-              channel = 0x2;
-              break;
-            case 0x37:
-              note = 0x2A;
-              channel = 0x3;
-              break;
-            case 0x3C:
-              note = 0x30;
-              channel = 0x3;
-              break;
-            case 0x48:
-              note = 0x3C;
-              channel = 0x3;
-              break;
-            default: break;
-          };
-          break;
-        case 0x4:  switch (note)
-          {
-            case 0x11:
-              note = 0x04;
-              channel = 0x4;
-              break;
-            case 0x13:
-              note = 0x07;
-              channel = 0x4;
-              break;
-            case 0x24:
-              note = 0x18;
-              channel = 0x4;
-              break;
-            case 0x30:
-              note = 0x24;
-              channel = 0x5;
-              break;
-            default: break;
-          };
-          break;
-        case 0x5:  switch (note)
-          {
-            case 0x30:
-              note = 0x24;
-              channel = 0x3;
-              break;
-            case 0x3E:
-              note = 0x32;
-              channel = 0x5;
-              break;
-            case 0x4E:
-              note = 0x42;
-              channel = 0x5;
-              break;
-            default: break;
-          };
-          break;
-        case 0xC:
-          switch (note)
-          {
-            case 0x0C:
-              note = 0x00;
-              channel = 0x4;
-              break;
-            default: break;
-          };
-          break;
-        default: break;
-      };
+        };
+        break;
+      case midi::ProgramChange:
+        switch (channel)
+        {
+          case 5:
+            data1 = 75;
+            break;
+          default: break;
+        };
+        break;
+      default: break;
     };
     // Thru on A has already pushed the input message to out A.
     // Forward the message to out B as well.
-    midiA.send(type, note, data2, channel);
+    midiA.send(type, data1, data2, channel);
   }
 #endif
 
